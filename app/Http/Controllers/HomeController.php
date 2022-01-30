d2754a233559ccbafdc96ffc72042663d30f0d88
riverbook708 <riverbook708@gmail.com>
2022-01-30 15:26:06 +0900

    basic MVC

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Memo;
+use App\Tag;
 
 class HomeController extends Controller
 {
@@ -26,7 +27,7 @@ class HomeController extends Controller
     {
         $user = \Auth::user();
         $memos = Memo::where('user_id', $user['id'])->where('status', 1)->orderby('updated_at', 'DESC')->get();
-        return view('home', compact('user', 'memos'));
+        return view('create', compact('user', 'memos'));
     }
 
     public function create()
@@ -39,13 +40,23 @@ class HomeController extends Controller
     public function store(Request $request)
     {
         $data = $request->all();
-        //dd($data);
+
+        $exist_tag = Tag::where('name',$data['tag'])->where('user_id',$data['user_id'])->first();
+        if(empty($exist_tag['id'])){
+            $tag_id = Tag::insertGetId(['name' => $data['tag'], 'user_id' => $data['user_id']]);
+        } else {
+            $tag_id = $exist_tag['id'];
+        }
+        //dd($exist_tag);
+
+        //dd($tag_id);
         // POSTされたデータをDB（memosテーブル）に挿入
         // MEMOモデルにDBへ保存する命令を出す
 
         $memo_id = Memo::insertGetId([
             'content' => $data['content'],
              'user_id' => $data['user_id'],
+             'tag_id' => $tag_id,
              'status' => 1
         ]);
 
@@ -58,17 +69,24 @@ class HomeController extends Controller
         $user = \Auth::user();
         $memo = Memo::where('status', 1)->where('id', $id)->where('user_id', $user['id'])->first();
         $memos = Memo::where('user_id', $user['id'])->where('status', 1)->orderby('updated_at', 'DESC')->get();
-        return view('edit', compact('user', 'memo' ,'memos'));
+        $tags = Tag::where('user_id', $user['id'])->get();
+        return view('edit', compact('user', 'memo' ,'memos', 'tags'));
     }
 
-    public function update(Request $request,$id)
+    public function update(Request $request, $id)
     {
         $inputs = $request->all();
-        Memo::where('id', $id)->update(['content' => $inputs['content']]);
+        //dd($inputs);
+        Memo::where('id', $id)->update(['content' => $inputs['content'], 'tag_id' => $inputs['tag_id']]);
         return redirect()->route('home');
     }
 
-
-
+    public function delete(Request $request, $id)
+    {
+        $inputs = $request->all();
+        //dd($inputs);
+        Memo::where('id', $id)->update(['status' => 2]);
+        return redirect()->route('home')->with('success', 'メモの削除が完了しました');
+    }
 
 }
